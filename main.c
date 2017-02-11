a4c242e8dbedd6eb6b0a7ffd50ab2319003c7114
MichaelTong <tonghao1992@gmail.com>
2017-02-11 00:07:39 -0600

    update

--- main.c
+++ main.c
@@ -11,10 +11,10 @@ typedef struct BigMEMBlock {
   unsigned char onebyte[1024*1024];
 } BigMEMBlock;
 
-int numblocks = 1024;
-int numworkers = 16;
+int numblocks = 1024*1024*1024/1024/1024 ;
+int numworkers = 2;
 int runtime = 15;
-
+int prepare = 5;
 
 size_t MEMBLOCKSIZE = sizeof(BigMEMBlock);
 BigMEMBlock *mempool;
@@ -24,16 +24,19 @@ FILE* logfd;
 pthread_t timer_td;
 int shouldend = 0;
 int background = 0;
+int record = 0;
 
 long setmem() {
   int idx = rand() % numblocks;
-  unsigned char newvalue = rand() % 256;
+  //unsigned char newvalue = rand() % 256;
   struct timespec begin, end;
   long timediff;
-  BigMEMBlock *target = mempool+idx;
+  BigMEMBlock *source = mempool+idx;
+  BigMEMBlock tmp;
 
   clock_gettime(CLOCK_MONOTONIC, &begin);
-  memset(target, newvalue, sizeof(MEMBLOCKSIZE));
+  memcpy(&tmp, source, sizeof(MEMBLOCKSIZE)); 
+  //memset(target, newvalue, sizeof(MEMBLOCKSIZE));
   clock_gettime(CLOCK_MONOTONIC, &end);
   timediff = (end.tv_sec - begin.tv_sec) * 1000000000 + (end.tv_nsec - begin.tv_nsec);
   return timediff;
@@ -56,23 +59,31 @@ void *dowork() {
   int timeidx = 0;
   while(!shouldend) {
     timediff = setmem();
-    if (!background && timeidx < 10000) {
+    if (record && !background && timeidx < 10000) {
       timerecords[timeidx] = timediff;
       timeidx ++;
     }
-    if (!background && timeidx == 10000) {
+    if (record && !background && timeidx == 10000) {
       flushlog(timerecords, 10000);
       timeidx = 0;
     }
   }
-  if (!background && timeidx != 0) {
-    flushlog(timerecords, 10000);
+  if (record && !background && timeidx != 0) {
+    flushlog(timerecords, timeidx);
   }
   return NULL;
 }
 
 void *timerend() {
   int i;
+  for (i = 0; background || i < prepare; i++) {
+    if (!background) {
+      printf("Prepare: %d/%d                \r", i, prepare);
+      fflush(stdout);
+    }
+    sleep(1);
+  }
+  record = 1;
   for (i = 0; background || i < runtime; i++) {
     if (!background) {
       printf("Progress: %d/%d               \r", i, runtime);
--- test.sh
+++ test.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
-
-bg=16
-for i in $(seq $bg); do
+for bg in 0 1 2 4 8 16 32; do
+  echo $bg
+  for i in $(seq $bg); do
     j=$(($i-1))
     ./cachetest 1 &
     pids[$j]=$!
-done
-./cachetest
-
-for i in $(seq $bg); do
+  done
+  sleep 15
+  perf stat -e faults ./cachetest &
+  wait $!
+  for i in $(seq $bg); do
     j=$(($i-1))
     echo "kill background: ${pids[$j]}"
     kill -9 ${pids[$j]}
+  done
+  mv memaccess.log memaccess.bg.${bg}.log
+  sleep 5
 done
+
