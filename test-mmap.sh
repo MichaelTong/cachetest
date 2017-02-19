891677fdf9dfb664be0b56ad1558d805c1b9b96b
MichaelTong <tonghao1992@gmail.com>
2017-02-18 22:28:08 -0600

    do not use sudo

--- test-mmap.sh
+++ test-mmap.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 for bg in 0 1 2 4 8 16 32; do
     if [ ! -f random-$bg.img ]; then
-        sudo dd if=/dev/urandom of=random-$bg.img bs=1M count=1024
+        dd if=/dev/urandom of=random-$bg.img bs=1M count=1024
     fi
 done
 
