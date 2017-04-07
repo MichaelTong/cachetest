54a3ec8322af2ae2cd5c9b9ef8649dc96510254e
MichaelTong <tonghao1992@gmail.com>
2017-04-06 23:27:57 -0500

    update

--- evict-cache.c
+++ evict-cache.c
@@ -13,26 +13,35 @@
 #include <errno.h>
 #include <string.h>
 
+#define PAGESIZE 4096
+
 int main(int argc, char** argv) {
-  int i;
-  unsigned long long length, pages;
+  unsigned long long i, length, numpages;
   int fd;
   char * mfd;
   char *evict_start;
+  char * filename = argv[1];
+  double percentage = atof(argv[2]);
+  srand((unsigned) time(NULL));
 
-  fd = open("imgs/random-1.img", O_NOATIME | O_RDONLY);
+  fd = open(filename, O_NOATIME | O_RDONLY);
   length = lseek(fd, 0, SEEK_END);
-  pages = length/4096;
-  printf("length %lld\n", length);
+  numpages = length/PAGESIZE;
+  //  printf("length %lld\n", length);
   lseek(fd, 0, SEEK_SET);
   mfd = mmap(NULL, length, PROT_READ, MAP_SHARED, fd, 0);
 
-  evict_start = mfd + pages/4*4096;
-  if (posix_fadvise(fd, length/4, length/2, POSIX_FADV_DONTNEED))
+  for (i = 0; i < numpages; i++) {
+    int rn = rand() % 10000;
+    if (rn < 100 * percentage) {
+      posix_fadvise(fd, i*PAGESIZE, PAGESIZE, POSIX_FADV_DONTNEED);
+    }
+  }
+  /*  if (posix_fadvise(fd, length/4, length/2, POSIX_FADV_DONTNEED))
     printf("unable to posix_fadvise file %s (%s)", "xxx", strerror(errno));
   if(msync(evict_start, pages/2*4096, MS_INVALIDATE)) {
     perror("");
-  }
+    }*/
   munmap(mfd, length);
   close(fd);
 }
